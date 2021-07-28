99f419c9143a3533b7f7009f072ca06b827f4268
josh <joshtupas@gmail.com>
2021-07-27 19:44:49 -0500

    added favicon stuff

--- app/seeds/cards.py
+++ app/seeds/cards.py
@@ -1,22 +1,74 @@
 # from werkzeug.security import generate_password_hash
-from app.models import db, User
+from app.models import db, Card
+
+cards = [
+    'A-C',
+    'A-D',
+    'A-H',
+    'A-S',
+    '2-C',
+    '2-D',
+    '2-H',
+    '2-S',
+    '3-C',
+    '3-D',
+    '3-H',
+    '3-S',
+    '4-C',
+    '4-D',
+    '4-H',
+    '4-S',
+    '5-C',
+    '5-D',
+    '5-H',
+    '5-S',
+    '6-C',
+    '6-D',
+    '6-H',
+    '6-S',
+    '7-C',
+    '7-D',
+    '7-H',
+    '7-S',
+    '8-C',
+    '8-D',
+    '8-H',
+    '8-S',
+    '9-C',
+    '9-D',
+    '9-H',
+    '9-S',
+    '10-C',
+    '10-D',
+    '10-H',
+    '10-S',
+    'J-C',
+    'J-D',
+    'J-H',
+    'J-S',
+    'Q-C',
+    'Q-D',
+    'Q-H',
+    'Q-S',
+    'K-C',
+    'K-D',
+    'K-H',
+    'K-S',
+]
 
 
-# Adds a demo user, you can add other users here if you want
-def seed_users():
-
-    demo = User(username='Demo', email='demo@aa.io',
-                password='password')
-
-    db.session.add(demo)
+def seed_cards():
+    for value in cards:
+        card = Card(face=value)
+        db.session.add(card)
 
     db.session.commit()
 
 
-# Uses a raw SQL query to TRUNCATE the users table.
+# Uses a raw SQL query to TRUNCATE the cards table.
 # SQLAlchemy doesn't have a built in function to do this
 # TRUNCATE Removes all the data from the table, and resets
 # the auto incrementing primary key
-def undo_users():
-    db.session.execute('TRUNCATE users RESTART IDENTITY CASCADE;')
+def undo_cards():
+    db.session.execute('TRUNCATE cards RESTART IDENTITY CASCADE;')
     db.session.commit()
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,14 +1,30 @@
 # from werkzeug.security import generate_password_hash
+from faker import Faker
 from app.models import db, User
 
+fake = Faker()
 
 # Adds a demo user, you can add other users here if you want
-def seed_users():
 
-    demo = User(username='Demo', email='demo@aa.io',
-                password='password')
+
+def seed_users():
+    demo = User(
+        username='Demo',
+        email='demo@user.io',
+        password='password'
+    )
 
     db.session.add(demo)
+
+    for _ in range(20):
+        user_profile = fake.simple_profile()
+        user = User(
+            username=user_profile['username'],
+            email=user_profile['mail'],
+            password=fake.password(length=10)
+        )
+
+        db.session.add(user)
 
     db.session.commit()
 
