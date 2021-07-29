88dcf6b2a6aa036759b868b70fe9dc9be2fff32d
josh <joshtupas@gmail.com>
2021-07-29 15:14:24 -0500

    initialized Menu component

---
+++ react-app/src/components/SinglePlayer/Menu/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Menu() {
+  return (
+    <>
+      <h1>--Title--</h1>
+      <h3>Score</h3>
+      <button>New Game</button>
+      <button>Load Game</button>
+    </>
+  );
+}
+
+export default Menu;
