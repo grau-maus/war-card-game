40db11d4cd404786e23c3ba44ab4444d4dc64d8c
josh <joshtupas@gmail.com>
2021-07-29 18:07:50 -0500

    added played card column in War model

--- app/models/war.py
+++ app/models/war.py
@@ -11,15 +11,19 @@ class War(db.Model):
     player2_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     player1_deck = db.Column(db.String)
     player2_deck = db.Column(db.String)
+    player1_played_card = db.Column(db.String)
+    player2_played_card = db.Column(db.String)
 
     def p1_deck(self):
         return {
             "id": self.id,
             "deck": self.player1_deck,
+            "p1PlayedCard": self.player1_played_card
         }
 
     def p2_deck(self):
         return {
             "id": self.id,
             "deck": self.player2_deck,
+            "p2PlayedCard": self.player2_played_card
         }
--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -1,9 +1,8 @@
 import React from "react";
-import test from "../../images/cards/2C.png";
+
 function Splash() {
   return (
     <>
-      <img src={test} alt='' />
       <h1>Splash page yo</h1>
     </>
   );
