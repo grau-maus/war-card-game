b26fb3bf1e61730de53812778105affe9e5e6651
josh <joshtupas@gmail.com>
2021-07-29 14:51:02 -0500

    SinglePlayer component skeleton initial setup

--- react-app/src/components/SinglePlayer/index.js
+++ react-app/src/components/SinglePlayer/index.js
@@ -2,11 +2,31 @@ import React from "react";
 import { connect } from "react-redux";
 
 function SinglePlayer(props) {
+  const { user } = props;
+
   return (
     <>
-      <h1>single player, yay</h1>
+      <h2>{"<"}computer profile image{">"}</h2>
+      <h1>computer</h1>
+      <h2>deck</h2>
+      <h2>drawn card</h2>
+      <h1>-------------------</h1>
+      <h2>drawn card</h2>
+      <h2>deck</h2>
+      <h1>{user.username}</h1>
+      <h2>{"<"}user profile image{">"}</h2>
+      <button>draw</button>
+      <button>fold</button>
     </>
   );
 }
 
-export default connect()(SinglePlayer);
+const mapStateToProps = (state) => ({
+  user: state.session.user
+});
+
+const mapDispatchToProps = (dispatch) => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SinglePlayer);
